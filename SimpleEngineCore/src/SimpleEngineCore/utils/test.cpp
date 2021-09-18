75854506c0a7c2a99a898a49b5ec8b48040f5ee9
andrew <maryskiy2@gmail.com>
2021-09-18 10:47:09 +0500

    created main project structure

---
+++ SimpleEngineCore/src/SimpleEngineCore/utils/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+namespace SimpleEngine
+{
+    void sayHello()
+    {
+        std::cout << "hello from simple engine core library" << std::endl;
+    }
+}
---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SimpleEngineCore/utils/test.hpp"
+
+int main() 
+{
+    std::cout << "hello from simple engine editor lib" << std::endl;
+    SimpleEngine::sayHello();
+    return 0;
+}
